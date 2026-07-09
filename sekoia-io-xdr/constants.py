318afdcba5f0f6706e188e830b123cd2ec503070
Clement Burtscher <clement.burtscher@sekoia.io>
2026-07-09 12:53:59 +0000

    Derive other urls from base one

--- sekoia-io-xdr/constants.py
+++ sekoia-io-xdr/constants.py
@@ -2,8 +2,8 @@ INTEGRATION_NAME = "SEKOIAOperationCenter"
 
 BASE_URL = "https://app.sekoia.io"
 
-OPERATION_CENTER_BASE_URL = "https://app.sekoia.io/api/v1/sic/alerts"
+OPERATION_CENTER_BASE_URL = f"{BASE_URL}/api/v1/sic/alerts"
 
-ASSETS_BASE_URL = "https://app.sekoia.io/api/v1/asset-management/assets"
+ASSETS_BASE_URL = f"{BASE_URL}/api/v1/asset-management/assets"
 
-CASES_BASE_URL = "https://app.sekoia.io/api/v1/sic/cases"
+CASES_BASE_URL = f"{BASE_URL}/api/v1/sic/cases"
